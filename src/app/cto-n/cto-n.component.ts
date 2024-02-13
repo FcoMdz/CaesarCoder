b0f6fbdbec13f625375d442f458d9c6d4c002778
FcoMdz <al289539@edu.uaa.mx>
2024-02-13 11:44:14 -0600

    primer commit

---
+++ src/app/cto-n/cto-n.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cto-n',
+  standalone: true,
+  imports: [],
+  templateUrl: './cto-n.component.html',
+  styleUrl: './cto-n.component.css'
+})
+export class CtoNComponent {
+
+}
